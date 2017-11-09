1c34f927bf0d22ec1c2ddc17e5dda07cf44ceee6
bunderwood <bunderwood@ciphr.com>
2017-11-09 00:26:50 +0000

    preperation for authentication with const authLink to provide token in query header

--- src/services/cmsapi.js
+++ src/services/cmsapi.js
@@ -1,11 +1,25 @@
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
 const httpLink = new HttpLink({
   // You should use an absolute URL here
   uri: 'http://localhost:3010/graphql'
+})
+
+// Create authentication header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  }
 })
 
 // Create subscription websocket link
@@ -30,12 +44,13 @@ const link = split(
       operation === 'subscription'
   },
   wsLink,
-  httpLink
+  httpLink,
+  authLink
 )
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  link,
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
